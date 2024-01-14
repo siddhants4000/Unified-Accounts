1e6d8e7601058d7c178025cd2bcea8d6fe6e3fa5
Siddhant <siddhants4000@gmail.com>
2024-01-14 22:28:07 +0530

    auth removed

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,58 +1,58 @@
-package com.example.demo.controller;
-
-import com.example.demo.config.JwtUtil;
-import com.example.demo.entity.User;
-import com.example.demo.repo.UserRepository;
-import com.example.demo.request.LoginRequest;
-import com.example.demo.response.ErrorResponse;
-import com.example.demo.response.LoginResponse;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.*;
-
-@CrossOrigin(origins = "http://localhost:3000/")
-@RestController
-@RequestMapping("/rest/auth")
-public class AuthController {
-
-    private final AuthenticationManager authenticationManager;
-
-    @Autowired
-    UserRepository userRepository;
-
-    private JwtUtil jwtUtil;
-    public AuthController(AuthenticationManager authenticationManager, JwtUtil jwtUtil) {
-        this.authenticationManager = authenticationManager;
-        this.jwtUtil = jwtUtil;
-
-    }
-
-    @ResponseBody
-    @RequestMapping(value = "/login",method = RequestMethod.POST)
-    public ResponseEntity login(@RequestBody LoginRequest loginReq)  {
-
-        try {
-
-            Authentication authentication =
-                    authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginReq.getEmail(), loginReq.getPassword()));
-            String email = authentication.getName();
-            User user = userRepository.findByUserEmail(email);
-            String token = jwtUtil.createToken(user);
-            LoginResponse loginRes = new LoginResponse(email,token);
-
-            return ResponseEntity.ok(loginRes);
-
-        }catch (BadCredentialsException e){
-            ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST,"Invalid username or password");
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
-        }catch (Exception e){
-            ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST, e.getMessage());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
-        }
-    }
-}
+//package com.example.demo.controller;
+//
+//import com.example.demo.config.JwtUtil;
+//import com.example.demo.entity.User;
+//import com.example.demo.repo.UserRepository;
+//import com.example.demo.request.LoginRequest;
+//import com.example.demo.response.ErrorResponse;
+//import com.example.demo.response.LoginResponse;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.authentication.BadCredentialsException;
+//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.web.bind.annotation.*;
+//
+//@CrossOrigin(origins = "http://localhost:3000/")
+//@RestController
+//@RequestMapping("/rest/auth")
+//public class AuthController {
+//
+//    private final AuthenticationManager authenticationManager;
+//
+//    @Autowired
+//    UserRepository userRepository;
+//
+//    private JwtUtil jwtUtil;
+//    public AuthController(AuthenticationManager authenticationManager, JwtUtil jwtUtil) {
+//        this.authenticationManager = authenticationManager;
+//        this.jwtUtil = jwtUtil;
+//
+//    }
+//
+//    @ResponseBody
+//    @RequestMapping(value = "/login",method = RequestMethod.POST)
+//    public ResponseEntity login(@RequestBody LoginRequest loginReq)  {
+//
+//        try {
+//
+//            Authentication authentication =
+//                    authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginReq.getEmail(), loginReq.getPassword()));
+//            String email = authentication.getName();
+//            User user = userRepository.findByUserEmail(email);
+//            String token = jwtUtil.createToken(user);
+//            LoginResponse loginRes = new LoginResponse(email,token);
+//
+//            return ResponseEntity.ok(loginRes);
+//
+//        }catch (BadCredentialsException e){
+//            ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST,"Invalid username or password");
+//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+//        }catch (Exception e){
+//            ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST, e.getMessage());
+//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+//        }
+//    }
+//}
